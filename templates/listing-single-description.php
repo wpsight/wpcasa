6cf1b131b5d81b3aff215e925ae63566ec96b17b
Simon Rimkus <simon@wpsight.com>
2015-08-31 16:54:16 +0200

    Mark unavailable items as such

--- templates/listing-single-description.php
+++ templates/listing-single-description.php
@@ -7,6 +7,12 @@ global $listing; ?>
 <div class="wpsight-listing-section wpsight-listing-section-description">
 	
 	<?php do_action( 'wpsight_listing_single_description_before', $listing->ID ); ?>
+	
+	<?php if( wpsight_is_listing_not_available() ) : ?>
+		<div class="wpsight-alert wpsight-alert-small wpsight-alert-not-available">
+			<?php _e( 'This property is currently not available.', 'wpsight' ); ?>
+		</div>
+	<?php endif; ?>
 
 	<div class="wpsight-listing-description" itemprop="description">
 		<?php echo apply_filters( 'wpsight_listing_description', wpsight_format_content( $listing->post_content ) ); ?>

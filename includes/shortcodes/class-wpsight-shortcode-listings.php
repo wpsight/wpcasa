fbf952a0966729e3d220308fb419c8fb049b61cf
Simon Rimkus <simon@wpsight.com>
2015-10-09 09:54:40 +0200

    Offer option for get_listings() to ignore get_query_vars for secondary queries

--- includes/shortcodes/class-wpsight-shortcode-listings.php
+++ includes/shortcodes/class-wpsight-shortcode-listings.php
@@ -31,42 +31,57 @@ class WPSight_Shortcode_Listings {
 	 */
 
     public function shortcode_listings( $atts ) {
+	    
+	    $atts['show_panel']		= isset( $atts['show_panel'] ) && $atts['show_panel'] == 'false' ? false : true;
+	    $atts['show_paging']	= isset( $atts['show_paging'] ) && $atts['show_paging'] == 'false' ? false : true;
         
         $defaults = array(
             'before'			=> '',
             'after'				=> '',
             'wrap'	 			=> 'div',
             'offset'			=> '',
-			'posts_per_page'	=> get_query_var( 'nr' ) ? get_query_var( 'nr' ) : get_option( 'posts_per_page' ),
-			'orderby'			=> get_query_var( 'orderby' ) ? get_query_var( 'orderby' ) : 'date',
-			'order'				=> get_query_var( 'order' ) ? get_query_var( 'order' ) : 'DESC',
+			'nr'				=> '',
+			'orderby'			=> 'date',
+			'order'				=> 'desc',
+			'class'		 		=> '', // additional css class
+			'orientation'		=> 'horizontal', // can be vertical
 			'show_panel'		=> true, // can be false
 			'show_paging'		=> true // can be false
         );
         
 		// Add custom vars to $defaults
-		$defaults = array_merge( $defaults, wpsight_listing_query_vars() );
+		
+		if( $atts['show_panel'] || $atts['show_paging'] )
+			$defaults = array_merge( $defaults, wpsight_listing_query_vars() );
 
 		// Merge shortcodes atts with defaults and extract
         extract( shortcode_atts( $defaults, $atts ) );
 		
 		$args = shortcode_atts( $defaults, $atts );
-        
-        // Optionally Convert strings true|false to bool
 		
-		$args['show_panel'] = $args['show_panel'] === true || $args['show_panel'] == 'true' ? true : false;
-		$args['show_paging'] = $args['show_paging'] === true || $args['show_paging'] == 'true' ? true : false;
+		// Respect orderby panel
+		
+		$args['orderby']	= $atts['show_panel'] && ! empty( get_query_var( 'orderby' ) ) ? get_query_var( 'orderby' ) : $args['orderby'];
+		$args['order']		= $atts['show_panel'] && ! empty( get_query_var( 'orderby' ) ) ? get_query_var( 'order' ) : $args['order'];
+		
+		// Get the listings
         
         ob_start();
         
         wpsight_listings( $args );
         
         $output = sprintf( '%1$s%3$s%2$s', $before, $after, ob_get_clean() );
+        
+        // Set css class
+		$class = ! empty( $class ) ? ' ' . sanitize_html_class( $class ) : '';
+		
+		// Set orientation
+		$orientation = in_array( $orientation, array( 'horizontal', 'vertical' ) ) ? ' ' . $orientation : '';
 	
 		// Optionally wrap shortcode in HTML tags
 		
 		if( ! empty( $wrap ) && $wrap != 'false' && in_array( $wrap, array_keys( wp_kses_allowed_html( 'post' ) ) ) )
-			$output = sprintf( '<%2$s class="wpsight-listings-sc">%1$s</%2$s>', $output, $wrap );
+			$output = sprintf( '<%2$s class="wpsight-listing-sc%3$s%4$s">%1$s</%2$s>', $output, $wrap, $class, $orientation );
 
         return apply_filters( 'wpsight_shortcode_listings', $output, $atts );
         

bf8b6809d88f135e2843943d18c51e7840bbea3b
Igor Chernenko <igorchernenko92@gmail.com>
2019-10-07 09:38:22 +0300

    ADD: params to details template

--- wpcasa/templates/listing-single-details.php
+++ wpcasa/templates/listing-single-details.php
@@ -2,14 +2,20 @@
 /**
  * Template: Single Listing Details
  */
-global $listing; ?>
+global $listing;
+
+$id =  isset($id) ? $id : $listing->ID;
+$formatted =  isset($formatted) ? $formatted : 'wpsight-listing-details';
+$details =  isset($details) ? $details : false;
+
+?>
 
 <div class="wpsight-listing-section wpsight-listing-section-details">
-	
-	<?php do_action( 'wpsight_listing_single_details_before', $listing->ID ); ?>
 
-	<?php wpsight_listing_details( $listing->ID ); ?>
-	
-	<?php do_action( 'wpsight_listing_single_details_after', $listing->ID ); ?>
+    <?php do_action( 'wpsight_listing_single_details_before', $id ); ?>
+
+    <?php wpsight_listing_details( $id, $details, $formatted ); ?>
+
+    <?php do_action( 'wpsight_listing_single_details_after', $id ); ?>
 
 </div><!-- .wpsight-listing-section-details -->

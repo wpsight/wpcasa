c4d96d9865056b0a9690ea3f3ae8b043ac05cf34
Igor Chernenko <igorchernenko92@gmail.com>
2019-12-05 11:36:20 +0200

    ADD: recommends

--- includes/admin/class-wpsight-admin-page-recommends.php
+++ includes/admin/class-wpsight-admin-page-recommends.php
@@ -15,7 +15,32 @@ class WPSight_Recommends {
 	 */
 	public function output()
     {
-        echo 'recommends';
+        $recommends = [
+            [
+                'title' =>  __( "Recommend title", "wpcasa" ),
+                'description' => __( "Description", "wpcasa" ),
+                'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Image-Porkeri_001.jpg',
+                'button_text' => __( "Button text", "wpcasa" ),
+                'button_link' => 'button link',
+            ],
+            [
+                'title' =>  __( "Recommend title2", "wpcasa" ),
+                'description' => __( "Description2", "wpcasa" ),
+                'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Image-Porkeri_001.jpg',
+                'button_text' => __( "Button text2", "wpcasa" ),
+                'button_link' => 'button link',
+            ],
+        ];
+
+        foreach($recommends as $key => $value) {
+            ?>
+
+            <div class="">
+                <h2><?php echo $value['image_url']; ?></h2>
+            </div>
+
+       <?php }
+
     }
 
 

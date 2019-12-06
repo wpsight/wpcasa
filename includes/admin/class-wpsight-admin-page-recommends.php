3a063af0f153c2ed24b9ecb95dde43807e3a739c
Happy-Child <lazuka_99@mail.ru>
2019-12-06 17:50:57 +0300

    ADD: recommends cards

--- includes/admin/class-wpsight-admin-page-recommends.php
+++ includes/admin/class-wpsight-admin-page-recommends.php
@@ -17,32 +17,49 @@ class WPSight_Recommends {
     {
         $recommends = [
             [
-                'title' =>  __( "Recommend title", "wpcasa" ),
-                'description' => __( "Description", "wpcasa" ),
+                'title' =>  __( "WPCasa Dashboard", "wpcasa" ),
+                'description' => __( "Let listing agents submit and manage property listings from the front end.", "wpcasa" ),
                 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Image-Porkeri_001.jpg',
                 'button_text' => __( "Button text", "wpcasa" ),
                 'button_link' => 'button link',
             ],
             [
-                'title' =>  __( "Recommend title2", "wpcasa" ),
-                'description' => __( "Description2", "wpcasa" ),
+                'title' =>  __( "WPCasa Listing PDF", "wpcasa" ),
+                'description' => __( "Let listing agents submit and manage property listings from the front end.", "wpcasa" ),
                 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Image-Porkeri_001.jpg',
                 'button_text' => __( "Button text2", "wpcasa" ),
                 'button_link' => 'button link',
             ],
-        ];
+        ]; ?>
 
-        foreach($recommends as $key => $value) {
-            ?>
+        <div class="wrap-cards-recommend">
 
-            <div class="">
-                <h2><?php echo $value['image_url']; ?></h2>
+        <?php foreach($recommends as $key => $value) { ?>
+
+            <div class="wrap-card">
+                <div class="card">
+                    <div class="card__wrap-img">
+                        <img src="<?php echo $value['image_url']; ?>" class="card__img" alt="">
+                    </div>
+
+                    <div class="card__content">
+                        <h2 class="card__wrap-link">
+                            <a href="<?php echo $value['button_link']; ?>" class="card__link"><?php echo $value['title']; ?></a>
+                        </h2>
+
+                        <p class="card__description"><?php echo $value['description']; ?></p>
+
+                        <span class="button card__label">€49,00 - €249,00</span>
+                    </div>
+                </div>
             </div>
 
-       <?php }
+        <?php } ?>
 
+        </div>
+
+    <?php
     }
-
 
 }
 

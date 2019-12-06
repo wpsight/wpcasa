c99c11ef4bf2e0ce9daffc2c5a24f622b73efcc3
Igor Chernenko <igorchernenko92@gmail.com>
2019-12-06 18:32:51 +0200

    EDIT: title

--- includes/admin/class-wpsight-admin-page-recommends.php
+++ includes/admin/class-wpsight-admin-page-recommends.php
@@ -33,7 +33,7 @@ class WPSight_Recommends {
         ]; ?>
 
         <div class="wrap-title-recommend">
-            <h2>WPCasa Recommends</h2>
+            <h2><?php __( "WPCasa Recommends", "wpcasa" ) ?></h2>
         </div>
 
         <div class="wrap-cards-recommend">

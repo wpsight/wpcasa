93ca9c0515f143ae7dea6b58652f5ae871bd80fb
Happy-Child <lazuka_99@mail.ru>
2020-01-23 20:51:29 +0300

    ADD: promo services

--- includes/admin/views/panel-promo-services.php
+++ includes/admin/views/panel-promo-services.php
@@ -1,3 +1,6 @@
-<div class="wpsight-admin-ui-image">
-    <a href="https://wpcasa.com" target="_blank"><img src="<?php echo WPSIGHT_PLUGIN_URL . '/assets/img/wpcasa-promo-image-services.jpg' ?>" /></a>
+<div class="wpsight-admin-promo-services promo-services">
+    <div class="promo-services-content">
+        <span class="promo-services-text">Need help with installation or Setup?<br> Or need something customized?</span>
+        <a href="#" class="promo-services-button">Request quote</a>
+    </div>
 </div>

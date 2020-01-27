cf28caa97c7273a8b364dd578ba1f6d9a850cf62
Igor Chernenko <igorchernenko92@gmail.com>
2020-01-27 18:22:02 +0200

    CHANGE: accaunt and support text and links

--- includes/admin/views/panel-support.php
+++ includes/admin/views/panel-support.php
@@ -1,26 +1,8 @@
-<?php
-$plugins	= get_plugins();
-$plugin		= 'wpcasa-assistance/wpcasa-assistance.php';
-?>
+<h3><?php _e( 'Support', 'wpcasa' ) ?></h3>
 
-<h3><?php _e( 'Assistance', 'wpcasa' ) ?></h3>
-
-<?php if( is_plugin_active( $plugin ) ) { ?>
-
-    <p><?php _e( 'If you cant find your question answered, feel free to submit a ticket to us.', 'wpcasa' ) ?></p>
-    <a href="#wpca" target="_blank" class="button button-primary button-text-icon"><span class="dashicons dashicons-sos"></span><?php _e( 'Submit Ticket', 'wpcasa' ) ?></a>
-
-<?php } else { ?>
-
-	<?php if( ! empty( $plugins[$plugin] ) ) { ?>
-        <p><?php _e( 'WPCasa Assistance is already installed. Please activate it in order to use it!', 'wpcasa' ) ?></p>
-        <a href="<?php $helpers = new WPSight_Admin_Helpers; echo $helpers->action_link( $plugin, 'activate' ); ?>" target="_self" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Activate', 'wpcasa' ) ?></a>
+<p><?php _e( 'In order to get help with an issue, please click the link below to submit a ticket', 'wpcasa' ) ?></p>
+    <?php if( wpsight_is_premium() ) { ?>
+        <a href="https://wpcasa.com/submit-ticket" target="_self" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Activate', 'wpcasa' ) ?></a>
     <?php } else { ?>
-        <p><?php _e( 'Please install and activate the WPCasa Assistance Plugin in order to receive support. ', 'wpcasa' ) ?></p>
-        <a href="<?php echo admin_url(); ?>plugin-install.php?s=wpcasa+assistance&tab=search&type=term" target="_self" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Install', 'wpcasa' ) ?></a>
+        <a href="https://wordpress.org/support/plugin/wpcasa/" target="_self" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Install', 'wpcasa' ) ?></a>
     <?php } ?>
-	    
-<?php } ?>
-
-
-

77c1a48d488217adb5e0981a7076ce901b0b098d
Igor Chernenko <igorchernenko92@gmail.com>
2020-02-21 14:18:57 +0200

    ADD: correct labels to button from the support title

--- includes/admin/views/panel-support.php
+++ includes/admin/views/panel-support.php
@@ -2,7 +2,7 @@
 
 <p><?php _e( 'In order to get help with an issue, please click the link below to submit a ticket', 'wpcasa' ) ?></p>
     <?php if( wpsight_is_premium() ) { ?>
-        <a href="https://wpcasa.com/submit-ticket" target="_blank" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Activate', 'wpcasa' ) ?></a>
+        <a href="https://wpcasa.com/submit-ticket" target="_blank" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Submit ticket', 'wpcasa' ) ?></a>
     <?php } else { ?>
-        <a href="https://wordpress.org/support/plugin/wpcasa/" target="_blank" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Install', 'wpcasa' ) ?></a>
+        <a href="https://wordpress.org/support/plugin/wpcasa/" target="_blank" class="button button-secondary button-text-icon"><span class="dashicons dashicons-upload"></span><?php _e( 'Submit ticket', 'wpcasa' ) ?></a>
     <?php } ?>

5a288e297e41c428d283ec949eacdfc009aebce5
Igor Chernenko <igorchernenko92@gmail.com>
2020-02-21 20:31:01 +0200

    ADD: customization link

--- includes/admin/views/panel-promo-services.php
+++ includes/admin/views/panel-promo-services.php
@@ -1,6 +1,6 @@
 <div class="wpsight-admin-promo-services promo-services">
     <div class="promo-services-content">
         <span class="promo-services-text"><?php _e( 'Need help with installation or Setup? Or need something customized?', 'wpcasa' ); ?></span>
-        <a href="#" class="promo-services-button"><?php _e( 'Request quote', 'wpcasa' ); ?></a>
+        <a href="https://wpcasa.com/customization-request/" target="_blank" class="promo-services-button"><?php _e( 'Request quote', 'wpcasa' ); ?></a>
     </div>
 </div>

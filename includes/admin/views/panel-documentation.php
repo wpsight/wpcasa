524a0ed3d093c8cd51df3a441e45a4edda141338
Igor Chernenko <igorchernenko92@gmail.com>
2020-03-17 17:10:04 +0200

    ADD: button color depending from color scheme

--- includes/admin/views/panel-documentation.php
+++ includes/admin/views/panel-documentation.php
@@ -1,3 +1,3 @@
 <h3><?php _e( 'Documentation', 'wpcasa' ) ?></h3>
 <p><?php _e( 'Check out our documentation where most of the settings and features are explained.', 'wpcasa' ) ?></p>
-<a href="https://docs.wpcasa.com/" target="_blank" class="button button-secondary button-text-icon"><span class="dashicons dashicons-editor-help"></span><?php _e( 'Read Documentation', 'wpcasa' ) ?></a>
+<a href="https://docs.wpcasa.com/" target="_blank" class="button button-primary button-text-icon"><span class="dashicons dashicons-editor-help"></span><?php _e( 'Read Documentation', 'wpcasa' ) ?></a>

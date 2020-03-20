d26afe697104f94dc2bf74960f75ead239a4720a
Igor Chernenko <igorchernenko92@gmail.com>
2020-03-20 15:48:02 +0200

    ADD: checks to constant

--- includes/wpcasa-admin-map-ui/wpcasa-admin-map-ui.php
+++ includes/wpcasa-admin-map-ui/wpcasa-admin-map-ui.php
@@ -24,8 +24,10 @@ class WPSight_Admin_Map_UI {
 		if ( ! defined( 'WPSIGHT_AUTHOR' ) )
 			define( 'WPSIGHT_AUTHOR', 'WPSight' );
 
-
+        if ( ! defined( 'WPSIGHT_ADMIN_MAP_UI_PLUGIN_DIR' ) )
 		define( 'WPSIGHT_ADMIN_MAP_UI_PLUGIN_DIR', WPSIGHT_PLUGIN_DIR . '/includes/wpcasa-admin-map-ui' );
+
+        if ( ! defined( 'WPSIGHT_ADMIN_MAP_UI_PLUGIN_URL' ) )
 		define( 'WPSIGHT_ADMIN_MAP_UI_PLUGIN_URL', WPSIGHT_PLUGIN_URL . '/includes/wpcasa-admin-map-ui' );
 
 		if ( is_admin() ){

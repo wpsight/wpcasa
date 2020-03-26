3c7f99e30194517e4e252d5d28d4b7054307f440
Igor Chernenko <igorchernenko92@gmail.com>
2020-03-26 19:14:51 +0200

    ADD: leaflet geocoder

---
+++ includes/leaflet/assets/js/main.js
@@ -0,0 +1,233 @@
+(function ($) {
+
+  /**
+   * Maps on DOM
+   * @type {Array}
+   */
+  var maps = [];
+
+  /**
+   * Default marker
+   */
+  var createMarker = function () {
+    return new L.Marker(null, { draggable: true });
+  };
+
+  /**
+   * Get value from lat-input
+   * @param context
+   * @returns {{from, to}|T|*|{line, ch}|{}}
+   */
+  var getLatField = function (context) {
+    return context.find('.leaflet-map__lat');
+  };
+
+  /**
+   * Get value from lng-input
+   * @param context
+   * @returns {{from, to}|T|*|{line, ch}|{}}
+   */
+  var getLngField = function (context) {
+    return context.find('.leaflet-map__lng');
+  };
+
+  /**
+   * Generate marker html-string
+   * @param markerCenter
+   * @returns {string}
+   */
+  var setMarkerHtml = function (markerCenter) {
+    return "<span class='leaflet-map__popup'>" + markerCenter.lat + ", " + markerCenter.lng + "</span>";
+  };
+
+  /**
+   * Add marker to map
+   * @param markerCenter
+   * @param map
+   */
+  var addMarker = function (markerCenter, map, marker) {
+    marker
+      .setLatLng(markerCenter)
+      .bindPopup(setMarkerHtml(markerCenter))
+      .addTo(map);
+
+    marker.openPopup();
+    marker.on('moveend', function (e) {
+      marker.openPopup();
+    });
+
+    // map.setView(markerCenter, CMB2LM.default_zoom);
+  };
+
+  /**
+   * Handle adding latLng to inputs
+   * @param e
+   * @param context
+   */
+  var handleLatLngChange = function (e, context) {
+    var latLng = e.layer ? e.layer.getLatLng() : new L.latLng(e.lat, e.lng);
+
+    getLatField(context).val(latLng.lat);
+    getLngField(context).val(latLng.lng);
+  };
+
+  /**
+   * Initialize Leaflet to map-container
+   * @param context
+   * @param index
+   */
+  var initMap = function (context, index) {
+
+    var mapId = 'cmb2-leaflet-map_' + index;
+    var marker = createMarker();
+
+    var latFieldVal = getLatField(context).val();
+    var lngFieldVal = getLngField(context).val();
+    // alert(CMB2LM.initial_coordinates.lat);
+    // alert(CMB2LM.initial_coordinates.lng);
+
+    var map = L.map(mapId, {
+      center: [
+        CMB2LM.initial_coordinates.lat,
+        CMB2LM.initial_coordinates.lng
+      ],
+      zoom: CMB2LM.initial_zoom
+    });
+
+    // if (latFieldVal && lngFieldVal) {
+    //   var markerCenter = new L.latLng(latFieldVal, lngFieldVal);
+    //   addMarker(markerCenter, map, marker);
+    // }
+
+
+    var tiles = L.esri.basemapLayer("Streets").addTo(map);
+
+    // create the geocoding control and add it to the map
+    var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+    // create an empty layer group to store the results and add it to the map
+    var results = L.layerGroup().addTo(map);
+
+
+
+
+
+
+    var search = BootstrapGeocoder.search({
+      inputTag: '_map_address',
+      placeholder: 'ex. LAX'
+    }).addTo(map);
+
+    search.on('results', function(data){
+      document.getElementById(mapId).style.display = 'block';
+      // document.getElementById('intro').style.display = 'none';
+
+      // http://leafletjs.com/reference.html#map-invalidatesize
+      // map.invalidateSize();
+      results.clearLayers();
+      for (var i = data.results.length - 1; i >= 0; i--) {
+        results.addLayer(L.marker(data.results[i].latlng));
+      }
+    });
+
+    // listen for the results event and add every result to the map
+    searchControl.on("results", function(data) {
+      results.clearLayers();
+      for (var i = data.results.length - 1; i >= 0; i--) {
+        results.addLayer(L.marker(data.results[i].latlng));
+      }
+    });
+
+
+    map.on('layeradd', function (e) {
+      return handleLatLngChange(e, context)
+    });
+
+
+
+   // If the map with given ID is already instantiated => return early
+   //  if (L.DomUtil.get(mapId)._leaflet_id) {
+    //   return;
+    // }
+    //
+    // var marker = createMarker();
+    // var geoCoder = new L.Control.Geocoder({
+    //   geocoder: null,
+    //   showResultIcons: false,
+    //   collapsed: true,
+    //   expand: 'click',
+    //   position: CMB2LM.searchbox_position,
+    //   placeholder: CMB2LM.search,
+    //   errorMessage: CMB2LM.not_found
+    // });
+    // var latFieldVal = getLatField(context).val();
+    // var lngFieldVal = getLngField(context).val();
+    // var map = L.map(mapId, {
+    //   center: [
+    //     CMB2LM.initial_coordinates.lat,
+    //     CMB2LM.initial_coordinates.lng
+    //   ],
+    //   zoom: CMB2LM.initial_zoom
+    // });
+    //
+    // L.tileLayer(CMB2LM.tilelayer, {
+    //   attribution: null
+    // }).addTo(map);
+    //
+    // geoCoder.addTo(map);
+    //
+    // // Check if fields are populated
+    // if (latFieldVal && lngFieldVal) {
+    //   var markerCenter = new L.latLng(latFieldVal, lngFieldVal);
+    //   addMarker(markerCenter, map, marker);
+    // }
+    //
+    // // Handle marker position add / change
+    // map.on('layeradd', function (e) {
+    //   return handleLatLngChange(e, context)
+    // });
+    //
+    // // Remove default markers on geocoder results
+    // map.on('geocoderMarkerAdd', function () {
+    //   return map.removeLayer(marker);
+    // });
+    //
+    // maps.push(map);
+
+  };
+
+  /**
+   * Initialize on load
+   */
+  $('.cmb-type-leaflet-map').each(function (i) {
+    $(this).find('.cmb2-leaflet__container').attr('id', 'cmb2-leaflet-map_' + i);
+    initMap($(this), i);
+  });
+
+  /**
+   * Handle new row add
+   */
+  // $('.cmb-repeatable-group').on('cmb2_add_row', function (event, newRow) {
+  //   var wrap = $(newRow).closest('.cmb-repeatable-group');
+  //
+  //   wrap.find('.cmb-row.cmb-repeatable-grouping').each(function (i) {
+  //     $(this).closest('.cmb2-leaflet__container').attr('id', 'cmb2-leaflet-map_' + i);
+  //     initMap($(this), i);
+  //   });
+  // });
+
+  /**
+   * Trigger invalidateSize() when meta box is opened
+   */
+  // $(document).on('postbox-toggled', function (e) {
+  //   for (var i = 0; i < maps.length; i++) {
+  //     var map = maps[i];
+  //     if (map && map._leaflet_id) {
+  //       var mapCenter = map.getCenter();
+  //       var mapZoom = map.getZoom();
+  //       map.invalidateSize();
+  //       map.setView(mapCenter, mapZoom);
+  //     }
+  //   }
+  // });
+})(jQuery);
---
+++ includes/leaflet/cmb-field-leaflet-map.php
@@ -0,0 +1,189 @@
+<?php
+
+/*
+Plugin Name: CMB2 Field Type: Leaflet Maps
+Plugin URI: https://github.com/villeristi/cmb2-field-leaflet-geocoder
+GitHub Plugin URI: https://github.com/villeristi/cmb2-field-leaflet-geocoder
+Description: Leaflet (with Geocoder) field type for CMB2.
+Version: 0.1.3
+Author: Ville Ristimäki
+Author URI: http://ville.io
+License: MIT
+*/
+
+class CMB2_Field_Leaflet {
+
+    /**
+     * @var string Version
+     */
+    const VERSION = '0.1.3';
+
+    /**
+     * @var string tilelayer
+     */
+    const INITIAL_TILELAYER = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+
+    /**
+     * @var string searchbox position
+     */
+    const SEARCHBOX_POSITION = 'topright'; // topright, bottomright, topleft, bottomleft
+
+    /**
+     * @var float latitude
+     */
+    const INITIAL_LAT = 61.9241;
+
+    /**
+     * @var float longitude
+     */
+    const INITIAL_LNG = 25.7482;
+
+    /**
+     * @var int initial zoomlevel
+     */
+    const INITIAL_ZOOM = 4;
+
+    /**
+     * @var int initial zoomlevel
+     */
+    const DEFAULT_ZOOM = 8;
+
+    /**
+     * CMB2_Field_Leaflet constructor.
+     */
+    public function __construct() {
+        add_filter( 'cmb2_render_leaflet_map', [ $this, 'render_leaflet_map' ], 10, 5 );
+        add_filter( 'cmb2_sanitize_leaflet_map', [ $this, 'sanitize_leaflet_map' ], 10, 4 );
+
+        // Add map and map args field to meta box
+        add_filter( 'wpsight_meta_box_listing_location_fields', array( $this, 'location_map_fields' ) );
+    }
+
+    /**
+     * Render the field
+     *
+     * @param $field
+     * @param $field_escaped_value
+     * @param $object_id
+     * @param $object_type
+     * @param $field_type_object
+     */
+    public function render_leaflet_map( CMB2_Field $field, $field_escaped_value, $object_id, $object_type, CMB2_Types $field_type_object ) {
+
+        $this->enqueue_scripts();
+        $this->localize_script( $field->args( 'attributes' ) );
+
+        $field_type_object->type = new CMB2_Type_Text( $field_type_object );
+
+
+        echo '<div id="geocode-selector"></div>';
+        echo "<div class='cmb2-leaflet__container'></div>";
+
+        $this->render_input( 'lat', $field, $field_escaped_value, $field_type_object );
+        $this->render_input( 'lng', $field, $field_escaped_value, $field_type_object );
+
+        $field_type_object->_desc( true, true );
+
+    }
+
+    /**
+     * Sanitize values
+     */
+    public function sanitize_leaflet_map( $override_value, $value, $object_id, $field_args ) {
+        //adapted admin map ui data to wpcasa map data
+        if ( ! empty( $value['lat'] ) ) update_post_meta( $object_id, '_geolocation_lat', $value['lat'] );
+        if ( ! empty( $value['lng'] ) ) update_post_meta( $object_id, '_geolocation_long', $value['lng'] );
+
+        return $value;
+    }
+
+    /**
+     * Enqueue scripts and styles
+     */
+    public function enqueue_scripts() {
+
+        wp_enqueue_script( 'cmb2-leaflet-core', '//unpkg.com/leaflet/dist/leaflet-src.js', [ 'jquery' ], self::VERSION );
+        wp_enqueue_style( 'cmb2-leaflet-core', '//unpkg.com/leaflet/dist/leaflet.css', [], self::VERSION );
+        wp_enqueue_style( 'cmb2-leaflet-geocoder_esri', '//unpkg.com/esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css', ['cmb2-leaflet-core'], self::VERSION );
+
+        wp_enqueue_script( 'cmb2-leaflet-geocoder_esri_main_js', 'https://unpkg.com/esri-leaflet', [], self::VERSION );
+        wp_enqueue_script( 'cmb2-leaflet-geocoder_esri_js', 'https://unpkg.com/esri-leaflet-geocoder', [], self::VERSION );
+
+        wp_enqueue_script( 'cmb2-leaflet-bootstrap-geocoder', plugins_url( 'assets/js/bootstrap-geocoder.js', __FILE__ ), [  ], self::VERSION );
+
+        wp_enqueue_script( 'cmb2-leaflet-main', plugins_url( 'assets/js/main.js', __FILE__ ), [ 'cmb2-leaflet-geocoder_esri_js' ], self::VERSION );
+        wp_enqueue_style( 'cmb2-leaflet-main', plugins_url( 'assets/css/style.css', __FILE__ ), [  ], self::VERSION );
+    }
+
+    /**
+     * @param $args
+     *
+     * @return bool
+     */
+    protected function localize_script( $args ) {
+        return wp_localize_script( 'cmb2-leaflet-main', 'CMB2LM', wp_parse_args( $args, [
+            'tilelayer'           => self::INITIAL_TILELAYER,
+            'searchbox_position'  => self::SEARCHBOX_POSITION,
+                'search'              => __( 'Search...', 'cmb2-leaflet-map' ),
+            'not_found'           => __( 'Not found', 'cmb2-leaflet-map' ),
+            'initial_coordinates' => [
+                'lat' => get_post_meta( get_the_ID(), '_geolocation_lat', true ),
+                'lng' => get_post_meta( get_the_ID(), '_geolocation_long', true )
+            ],
+            'initial_zoom'        => self::INITIAL_ZOOM,
+            'default_zoom'        => self::DEFAULT_ZOOM
+        ] ) );
+    }
+
+
+
+    /**
+     *	location_map_fields()
+     *
+     *	Register the location meta fields
+     *
+     *	@param	array  $fields
+     *	@uses	wpsight_sort_array_by_priority()
+     *	@return array
+     *
+     *	@since 1.0.0
+     */
+    public static function location_map_fields( $fields ) {
+        // Set meta box fields
+        $fields['location'] = array(
+            'id'       => '_map_geolocation',
+            'name'     => __( 'Location', 'wpcasa-admin-map-ui' ),
+            'desc'     => false,
+            'type'     => 'leaflet_map',
+            'priority' => 20
+        );
+
+        // Apply filter and order fields by priority
+        $fields = wpsight_sort_array_by_priority( apply_filters( 'wpsight_meta_box_listing_location_map_fields', $fields ) );
+
+        return $fields;
+    }
+
+    /**
+     * @param string     $field_name
+     * @param CMB2_Field $field
+     * @param            $field_escaped_value
+     * @param CMB2_Types $field_type_object
+     *
+     * @internal param array $args
+     */
+    protected function render_input( $field_name = '', CMB2_Field $field, $field_escaped_value, CMB2_Types $field_type_object ) {
+        $attrs = $field_type_object->concat_attrs( [
+            'id'    => "{$field->args( 'id' )}_{$field_name}",
+            'type'  => 'hidden',
+            'name'  => "{$field->args( '_name' )}[{$field_name}]",
+            'value' => isset( $field_escaped_value[ $field_name ] ) ? $field_escaped_value[ $field_name ] : '',
+            'class' => "leaflet-map__{$field_name}",
+            'desc'  => ''
+        ], [ 'attributes' ] );
+
+        echo sprintf( '<input%s />', $attrs );
+    }
+}
+
+new CMB2_Field_Leaflet();

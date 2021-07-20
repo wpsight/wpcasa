c3c043219638b9f5ab217f3d7d48a05227e728b8
Joe Hana <me@joehana.com>
2021-07-20 17:33:27 +0200

    Fix proper data escaping

--- includes/listings-map/templates/listings-map.php
+++ includes/listings-map/templates/listings-map.php
@@ -11,16 +11,16 @@
 	<?php if( ( true === $args['toggle'] || 'listings-panel' == $args['toggle_button'] ) && false == $args['map_page'] ) : ?>
 	
 		<?php if( ! empty( $args['toggle_button'] ) && 'listings-panel' != $args['toggle_button'] ) : ?>
-		<a href="#" class="toggle-map" data-toggle-map="<?php echo esc_attr( $args['map_id'] ); ?>"><span><?php echo strip_tags( $args['toggle_button'] ); ?></span></a>
+		<a href="#" class="toggle-map" data-toggle-map="<?php echo esc_attr( $args['map_id'] ); ?>"><span><?php echo esc_html( $args['toggle_button'] ); ?></span></a>
 		<?php endif; ?>
 	
 		<div id="map-toggle-<?php echo esc_attr( $args['map_id'] ); ?>" class="map-toggle">
-			<div id="<?php echo esc_attr( $args['map_id'] ); ?>" class="map-canvas" style="width: <?php echo $args['width']; ?>; height: <?php echo $args['height']; ?>"></div>
+			<div id="<?php echo esc_attr( $args['map_id'] ); ?>" class="map-canvas" style="width: <?php echo esc_attr( $args['width'] ); ?>; height: <?php echo esc_attr( $args['height'] ); ?>"></div>
 		</div>
 	
 	<?php else : ?>
 	
-		<div id="<?php echo esc_attr( $args['map_id'] ); ?>" class="map-canvas map-init" style="width: <?php echo $args['width']; ?>; height: <?php echo $args['height']; ?>"></div>
+		<div id="<?php echo esc_attr( $args['map_id'] ); ?>" class="map-canvas map-init" style="width: <?php echo esc_attr( $args['width'] ); ?>; height: <?php echo esc_attr( $args['height'] ); ?>"></div>
 	
 	<?php endif; ?>
 
